719d10e7a9aaf0b3a6926376b5ee29f59ff5a371
kwananak <100873735+kwananak@users.noreply.github.com>
2024-01-12 11:30:36 -0500

    bullets and beams fixes
    player can now move on tile where there's a bullet or a beam and tank the damage

--- Scripts/beam.gd
+++ Scripts/beam.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_area_entered(area):
 	if area.is_in_group("Player"):
-		get_tree().get_first_node_in_group("VirtualLevelManager").call_game_over()
+		get_tree().get_first_node_in_group("VirtualLevelManager").health -= 1
