851772c8114451f1c8d1c3950574082a7914afb5
kwananak <100873735+kwananak@users.noreply.github.com>
2024-01-24 20:31:30 -0500

    Revert "branched program_tile visuals to scene"
    This reverts commit 335197c7490eece0601bf22ddf7f6e2fc0b30970.

--- Scripts/Programs/program_tile.gd
+++ Scripts/Programs/program_tile.gd
@@ -5,17 +5,18 @@ var program_slot : String
 var program_type : String
 var program
 
-@onready var name_label = $ProgramVisual/NameLabel
-@onready var info_label = $ProgramVisual/InfoLabel
-@onready var animation_player = $ProgramVisual/AnimationPlayer
+@onready var name_label = $AnimatedSprite2D/NameLabel
+@onready var info_label = $AnimatedSprite2D/InfoLabel
+@onready var animation_player = $AnimatedSprite2D/AnimationPlayer
 
 func _ready():
 	program = load("res://Scenes/Programs/" + program_slot + "/" + program_type +  ".tscn").instantiate()
-	$ProgramVisual.add_child(program)
+	$AnimatedSprite2D.add_child(program)
 	name_label.text = program_type.to_upper()
 	info_label.text = program_slot.to_upper() + "\n\n" + program.info
 	program.position = Vector2(-13.5, -9.5)
 	program.scale = Vector2(1.25, 1.25)
+	program.monitorable = true
 
 # calls progress manager to add picked up program to the list
 func pick_up(_area):
@@ -23,13 +24,13 @@ func pick_up(_area):
 	program.set_deferred("monitorable", false)
 	get_tree().get_first_node_in_group("VirtualLevelManager").programs += [[program_slot, program]]
 	get_tree().get_first_node_in_group("MouseToolTip").hide()
-	$ProgramVisual.remove_child(program)
+	$AnimatedSprite2D.remove_child(program)
 	queue_free()
 
 func _on_mouse_entered():
 	animation_player.pause()
-	create_tween().tween_property($ProgramVisual, "scale", Vector2(1.5, 1.5), 0.2)
+	create_tween().tween_property(self, "scale", Vector2(5, 5), 0.2)
 
 func _on_mouse_exited():
 	animation_player.play()
-	create_tween().tween_property($ProgramVisual, "scale", Vector2(0.22, 0.22), 0.2)
+	create_tween().tween_property(self, "scale", Vector2(1, 1), 0.2)
