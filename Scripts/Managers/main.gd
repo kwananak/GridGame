c60b2e7d541915ba982ced773d5c3b4c4ae96c4d
kwananak <100873735+kwananak@users.noreply.github.com>
2024-01-28 22:26:46 -0500

    real background keeps playing in terminal
    workaround so that real level music keeps playing but lower behind terminal noise
    might be a bit overcomplicated

--- Scripts/Managers/main.gd
+++ Scripts/Managers/main.gd
@@ -9,6 +9,7 @@ var virtual_scene
 @onready var progress_manager = $ProgressManager
 @onready var continue_button = $Menu/ContinueGame
 @onready var new_game_button = $Menu/NewGame
+@onready var menu_audio = $MenuAudio
 
 func _ready():
 	# await progress_manager.load_game()
@@ -20,7 +21,7 @@ func _ready():
 
 # instantiates chosen level
 func call_level(level_number):
-	$AudioStreamPlayer.stop()
+	menu_audio.stop()
 	menu.visible = false
 	var level
 	if level_number == 100:
@@ -32,6 +33,8 @@ func call_level(level_number):
 	if level_number < 100:
 		real_scene = level
 	else:
+		if has_node("RealAudio"):
+			$RealAudio.stop()
 		virtual_scene = level
 	add_child(level)
 
@@ -44,9 +47,11 @@ func call_menu(level_number):
 		get_node("RealTestLevel").queue_free()
 	elif level_number < 100:
 		remove_child(real_scene)
+		$RealAudio.stop()
 	else:
 		get_node("Level" + str(level_number)).queue_free()
 	if real_scene and terminal_scene != null:
+		$RealAudio.play()
 		camera_2d.position = terminal_scene.position + get_viewport_rect().size / 4
 		terminal_scene.get_node("Control/Loadout").set_slots()
 		terminal_scene.visible = true
@@ -54,7 +59,7 @@ func call_menu(level_number):
 	else:
 		camera_2d.position = get_viewport_rect().size / 4
 		menu.visible = true
-		$AudioStreamPlayer.play()
+		menu_audio.play()
 		if real_scene:
 			continue_button.disabled = false
 			continue_button.grab_focus()
@@ -68,6 +73,7 @@ func call_quit():
 # instantiates terminal scene and hides + pauses real scene when called
 func call_terminal_scene(terminal_name):
 	remove_child(real_scene)
+	$RealAudio.volume_db = -25
 	if terminal_name == "TerminalTestScene":
 		terminal_scene = load("res://Scenes/terminal_test_scene.tscn").instantiate()
 	else:
@@ -78,6 +84,7 @@ func call_terminal_scene(terminal_name):
 # returns to real scene when closing terminal
 func return_to_real_scene():
 	add_child(real_scene)
+	$RealAudio.volume_db = -20
 	for n in get_tree().get_nodes_in_group("TerminalDoors"):
 		n.update_door()
 
@@ -102,6 +109,8 @@ func continue_game():
 	if !real_scene:
 		await progress_manager.load_game()
 		real_scene = load("res://Scenes/Levels/" + progress_manager.save_point + ".tscn").instantiate()
-	$AudioStreamPlayer.stop()
+	menu_audio.stop()
+	if has_node("RealAudio"):
+		$RealAudio.play()
 	menu.visible = false
 	add_child(real_scene)
--- Scripts/Managers/real_level_manager.gd
+++ Scripts/Managers/real_level_manager.gd
@@ -2,7 +2,15 @@ extends "res://Scripts/Managers/level_manager.gd"
 
 var turn = 0
 
+@onready var audio
+
 func _ready():
+	audio = get_parent().get_node("RealAudio")
+	get_parent().remove_child.call_deferred(audio)
+	if $/root/Main.has_node("RealAudio"):
+		$/root/Main.remove_child($/root/Main/RealAudio)
+	$/root/Main.add_child.call_deferred(audio)
+	audio.play()
 	player = get_tree().get_first_node_in_group("RealPlayer")
 	super._ready()
 
