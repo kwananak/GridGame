b946e7a0c4032908fba0c7541c4a08057c63b92e
kwananak <100873735+kwananak@users.noreply.github.com>
2024-02-10 19:24:08 -0500

    first pass at attack particles
    not good so far

--- Scripts/Environment/attack_careful_particle.gd
+++ Scripts/Environment/attack_careful_particle.gd
@@ -1,19 +1,14 @@
 extends "res://Scripts/Environment/careful_particle.gd"
 
 var player
+var seeking = false
 
 func _ready():
-	level_manager = get_tree().get_first_node_in_group("VirtualLevelManager")
-	match randi_range(0, 3):
-		0:
-			pass
-		1:
-			pass
-		2:
-			position = Vector2(level_manager.camera.position.x - get_viewport_rect().size.x / 4, randf_range(level_manager.camera.position.y - get_viewport_rect().size.x / 4, level_manager.camera.position.y + get_viewport_rect().size.x / 4))
-		3:
-			position = Vector2(level_manager.camera.position.x + get_viewport_rect().size.x / 4, randf_range(-10.0, level_manager.level_height * level_manager.tile_size + 10))
-	seeker()
+	player = get_tree().get_first_node_in_group("VirtualPlayer")
+	super._ready()
 
-func seeker():
-	pass
+func deviator():
+	seeking = true
+	while true:
+		await get_tree().create_timer(randf_range(1.0, 2.0)).timeout
+		direction = position.move_toward(player.position, 1.0) - position
--- Scripts/Environment/virtual_level_background.gd
+++ Scripts/Environment/virtual_level_background.gd
@@ -4,18 +4,17 @@ var paused = false
 var loaded_prefab
 var loaded_attack_prefab
 var level_manager
-var upper_range
+var upper_range = 1.0
 var spawning = true
 
 @onready var active_node = $Careful
 @onready var basic_particle_prefab = preload("res://Scenes/Tiles/basic_particle.tscn")
 @onready var careful_particle_prefab = preload("res://Scenes/Tiles/careful_particle.tscn")
 @onready var danger_particle_prefab = preload("res://Scenes/Tiles/danger_particle.tscn")
-@onready var careful_attack_particle_prefab = preload("res://Scenes/Tiles/careful_particle.tscn")
+@onready var attack_careful_particle_prefab = preload("res://Scenes/Tiles/attack_careful_particle.tscn")
 
 func _ready():
 	level_manager = get_tree().get_first_node_in_group("VirtualLevelManager")
-	upper_range = 1.0
 	if level_manager != null:
 		level_manager.pause_trigger.connect(set_pause)
 		level_manager.game_over_trigger.connect(set_pause)
@@ -37,12 +36,14 @@ func spawn_atttack_particles():
 	while true:
 		if !is_inside_tree():
 			break
-		await get_tree().create_timer(randf_range(0.1, upper_range)).timeout
+		await get_tree().create_timer(randf_range(1.0, 3.0)).timeout
 		if !paused:
 			add_child(loaded_attack_prefab.instantiate())
 
 func _on_careful_visibility_changed():
 	loaded_prefab = careful_particle_prefab
+	loaded_attack_prefab = attack_careful_particle_prefab
+	spawn_atttack_particles()
 
 func _on_danger_visibility_changed():
 	loaded_prefab = danger_particle_prefab
