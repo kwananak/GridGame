e31173955a74aa2bc57f8db7b452ce092ab175e3
kwananak <100873735+kwananak@users.noreply.github.com>
2024-03-19 17:28:22 -0400

    mouse work
    hides cursor when not in use
    shows when moving or clicking mouse
    tooltip can't go offscreen anymore and will hide on cursor hide

--- Scripts/Managers/framed_checker.gd
+++ Scripts/Managers/framed_checker.gd
@@ -15,3 +15,15 @@ func check(pos):
 	if pos.y > camera.position.y + get_viewport_rect().size.y / 4:
 		return false
 	return true
+
+func keep_in_frame(pos):
+	var return_vector = pos
+	if pos.x < camera.position.x - get_viewport_rect().size.x / 4:
+		return_vector.x = camera.position.x - get_viewport_rect().size.x / 4
+	if pos.x > camera.position.x + get_viewport_rect().size.x / 4:
+		return_vector.x = camera.position.x + get_viewport_rect().size.x / 4
+	if pos.y < camera.position.y - get_viewport_rect().size.y / 4:
+		return_vector.y = camera.position.y - get_viewport_rect().size.y / 4
+	if pos.y > camera.position.y + get_viewport_rect().size.y / 4:
+		return_vector.y = camera.position.y + get_viewport_rect().size.y / 4
+	return return_vector
--- Scripts/UI/mouse_tool_tip.gd
+++ Scripts/UI/mouse_tool_tip.gd
@@ -1,26 +1,46 @@
 extends Area2D
 
 var camera
+var framed_checker
+@onready var node_2d = $Node2D
+@onready var sprite_2d = $Sprite2D
+@onready var label = $Sprite2D/Label
 
 # gets camera reference on ready
 func _ready():
 	camera = get_tree().get_first_node_in_group("Camera")
+	framed_checker = get_tree().get_first_node_in_group("FramedChecker") 
 
 # updates position to mouse position on each frame
 func _process(_delta):
-	position = (get_viewport().get_mouse_position() / 2) + (camera.position - (get_viewport_rect().size / 4)) + Vector2(16, -32)
+	position = (get_viewport().get_mouse_position() / 2) + (camera.position - (get_viewport_rect().size / 4))
 	if has_overlapping_areas():
 		if get_overlapping_areas().is_empty():
 			return
 		var area = get_overlapping_areas()[0]
 		if area.get("info"):
+			if !framed_checker.check(node_2d.global_position):
+				sprite_2d.position = Vector2(20, 16) - (node_2d.global_position - framed_checker.keep_in_frame(node_2d.global_position))
+			else:
+				sprite_2d.position = Vector2(20, 16)
 			var adjusted_name = area.name
+			if "type" in area:
+				adjusted_name = area.type
 			while true:
 				if adjusted_name.right(1).is_valid_int():
 					adjusted_name = adjusted_name.left(-1)
 				else:
 					break
-			$Label.text = adjusted_name + "\n" + area.info
+			label.text = adjusted_name + "\n" + area.info
 			show()
 	else:
 		hide()
+
+func _input(event):
+	if event is InputEventMouse:
+		set_process(true)
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		set_process(false)
+		hide()
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
