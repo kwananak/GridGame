c10d383c246c9b821ef75f09873bdf91e3d55e0f
kwananak <100873735+kwananak@users.noreply.github.com>
2024-03-19 17:45:22 -0400

    added teleport sound

--- Scripts/Programs/Boots/sour.gd
+++ Scripts/Programs/Boots/sour.gd
@@ -1,6 +1,8 @@
 extends "res://Scripts/Programs/program.gd"
 
 var distance = 2
+@onready var up = $Up
+@onready var down = $Down
 
 func _ready():
 	type = "Sour"
@@ -24,24 +26,24 @@ func action():
 	player.move_check(distance)
 
 func confirm_with_dir(dir):
+	up.play()
 	for n in player.possible_moves:
 		n.hide()
 	var tele = dir.available_action
 	if tele:
 		tele.monitorable = false
 		tele.monitoring = false
-		#await create_tween().tween_property(tele, "scale", Vector2(0.5, 0.5), 0.05).finished
 		tele.hide()
 		tele.global_position = Vector2.ZERO
 	var pos = player.global_position
 	await player.move(dir.global_position)
+	down.play()
 	focus = false
 	player.waiting_for_action = null
 	player.teleport = false
 	if tele:
 		tele.global_position = pos
 		tele.show()
-		#create_tween().tween_property(tele, "scale", Vector2.ONE, 0.05)
 		tele.monitorable = true
 		tele.monitoring = true
 		if level_manager.astar_grid.is_point_solid(player.global_position / level_manager.tile_size):
