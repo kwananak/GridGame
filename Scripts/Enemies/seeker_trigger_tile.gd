7fb025a9febab8bf6066c47063613029874b0e30
kwananak <100873735+kwananak@users.noreply.github.com>
2024-03-22 18:46:23 -0400

    shields for enemies
    can be turned off in inspector
    also set recharge time in inspector

--- Scripts/Enemies/seeker_trigger_tile.gd
+++ Scripts/Enemies/seeker_trigger_tile.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var seeker_number = 0
 
-func _on_area_entered(area):
+func _on_area_entered(_area):
 	for n in get_tree().get_nodes_in_group("SeekerEnemies"):
 		if n.seeker_number == seeker_number:
 			n.activated = true
