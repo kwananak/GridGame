f27eb13e5612b4544ef71d6b67ee8358e1cdcabc
kwananak <100873735+kwananak@users.noreply.github.com>
2024-06-03 12:51:43 -0400

    added cutscene 4

--- Scripts/Cutscenes/robot_light.gd
+++ Scripts/Cutscenes/robot_light.gd
@@ -1,12 +1,9 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
 var counter = 0.0
-
-func _ready():
-	texture = texture.duplicate()
 
 func _process(delta):
 	counter -= delta
 	if counter < 0.0:
-		texture.region = Rect2(0.0, float(randi_range(0, 9) * 32), 32.0, 32.0)
+		frame = randi_range(0, sprite_frames.get_frame_count("default") - 1)
 		counter = randf_range(0.3, 2.0)
